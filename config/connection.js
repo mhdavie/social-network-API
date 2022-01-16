bd51dab5f4ed181419b5810af06c160eb95701d4
mdavie <marianahodniki1991@gmail.com>
2022-01-16 15:27:43 -0700

    deleted node modules

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-API';
+  process.env.MONGODB_URI || 'mongodb://localhost/social-network-API';
 
 connect(connectionString, {
   useNewUrlParser: true,
