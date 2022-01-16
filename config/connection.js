19b7014d7e4276eee5787d8ed3f024429fe7b596
mdavie <marianahodniki1991@gmail.com>
2022-01-16 12:09:23 -0700

    added api routes for thoughs

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/yourSiteName', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
