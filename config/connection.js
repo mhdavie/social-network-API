aa6698ca219724cf395061aa49649a3505b4f380
mdavie <marianahodniki1991@gmail.com>
2022-01-17 14:18:24 -0700

    debugging connetions

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,9 @@ const { connect, connection } = require('mongoose');
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://0.0.0.0/social-network-API';
 
-connect(connectionString)
-
+  connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
 module.exports = connection;
