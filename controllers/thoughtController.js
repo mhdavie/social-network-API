480a44f992c9ba1a6e4a78afba6d8601a38d3c26
mdavie <marianahodniki1991@gmail.com>
2022-01-20 21:11:37 -0700

    delete thought route fixed, added message

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -29,15 +29,15 @@ module.exports = {
 
     // update thought info 
     updateThought({ params, body }, res) {
-        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        Thought.findOneAndUpdate({ _id: params.Id }, body, { new: true, runValidators: true })
         .then(dbThoughtData =>  dbThoughtData ? res.json(dbThoughtData) : res.status(404).json({ message: thought404Message(params.id) }))
         .catch(err => res.status(400).json(err))
     },
 
     // delete thought 
     deleteThought({ params }, res) {
-        Thought.findOneAndDelete({ _id: params.id })
-        .then(dbThoughtData =>  dbThoughtData ? res.json(thought200Message(dbThoughtData._id)) : res.status(404).json({ message: thought404Message(params.id) }))
+        Thought.findOneAndDelete({ _id: params.Id })
+        .then(dbThoughtData =>  dbThoughtData ? res.json(thought200Message(dbThoughtData._id)) : res.status(404).json({ message: "this thought it was deleted" } ))
         .catch(err => res.status(404).json(err))
     },
 
@@ -71,7 +71,7 @@ module.exports = {
 
     // remove a reaction from thought
     removeReaction({ params }, res) {
-        Thought.findOneAndUpdate({ _id: params.thoughtId}, { $pull: { reactions: { _id: params.reactionId} } }, { new: true})
+        Thought.findOneAndUpdate({ _id: params.Id}, { $pull: { reactions: { _id: params.reactionId} } }, { new: true})
         .then(dbThoughtData =>  dbThoughtData ? res.json(reaction200Message(params.thoughtId)) : res.status(404).json({ message: "this reaction its deleted" }))
         .catch(err => res.status(404).json(err))
     }
